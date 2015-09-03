880f1079974701fbba2654dee367281c87f2fa4a
g2525_000 <g25259@live.com>
2015-09-03 20:56:44 +0800

    Accomplish ALZ

--- src/ActiveLzTree.java
+++ src/ActiveLzTree.java
@@ -14,17 +14,14 @@ public class ActiveLzTree {
 
     class Node {
         List<Node> children;
-        int numOfChildren;
         int fre;
         String activity;
 
         Node(String activity) {
             this.activity = activity;
+            children = new ArrayList<>();
         }
 
-        Node() {
-
-        }
     }
 
     public void init() {
@@ -38,74 +35,103 @@ public class ActiveLzTree {
         if (!seenActivity.contains(activity))
             seenActivity.add(activity);
 
-        if (findActivityNode(phase) == null) {
-            phase.add(activity);
+        List<String> newPattern = new ArrayList<>(phase);
+        newPattern.add(activity);
+        if (findActivityNode(newPattern) != root) {
+            phase = newPattern;
         } else {
-            addActivity(phase);
-            if (phase.size() > maxLength)
+            addActivity(newPattern);
+            if (newPattern.size() > maxLength)
                 ++maxLength;
             phase.clear();
         }
 
         window.add(activity);
-        if(window.size() > maxLength)
+        if (window.size() > maxLength)
             window.remove(0);
 
         incrementAllSuffixes();
     }
-    private void incrementAllSuffixes(){
+
+    private void incrementAllSuffixes() {
         List<List<String>> suffixList = getAllSuffixes();
 
         for (int i = 0; i < suffixList.size(); i++) {
-                incrementSuffix(suffixList.get(i));
+            incrementSuffix(suffixList.get(i));
         }
     }
-    private void incrementSuffix(List<String> suffix){
-        String lastActivity = suffix.remove(suffix.size()-1);
-        Node parentNode = findActivityNode(suffix);
-        Node node;
-        for (int i = 0; i < parentNode.numOfChildren; i++) {
 
+    private void incrementSuffix(List<String> suffix) {
+        String lastActivity = suffix.remove(suffix.size() - 1);
+        Node parentNode = findActivityNode(suffix);
+        Node node = null;
+        for (int i = 0; i < parentNode.children.size(); i++) {
+            Node tmp = parentNode.children.get(i);
+            if (tmp.activity.equals(lastActivity)) {
+                node = tmp;
+            }
         }
+        if (node == null) {
+            parentNode.children.add(new Node(lastActivity));
+            node = parentNode.children.get(parentNode.children.size() - 1);
+        }
+        //parentNode.fre++;
+        node.fre++;
     }
+
     private List<List<String>> getAllSuffixes() {
         List<List<String>> allSuffixes = new ArrayList<>();
         List<String> suffixes;
         for (int i = 0; i < window.size(); i++) {
             suffixes = new ArrayList<>();
-            for (int j = i; j < window.size() ; j++) {
-                suffixes.add(window.get(i));
+            for (int j = i; j < window.size(); j++) {
+                suffixes.add(window.get(j));
             }
             allSuffixes.add(suffixes);
         }
         return allSuffixes;
     }
-    private void addActivity(List<String> phase) {
+
+    private Node addActivity(List<String> phase) {
         if (phase.size() == 1) {
             root.children.add(new Node(phase.get(0)));
-            ++root.numOfChildren;
+            return root.children.get(root.children.size() - 1);
         } else {
             String lastActivity = phase.remove(phase.size() - 1);
             Node x = findActivityNode(phase);
             x.children.add(new Node(lastActivity));
-            ++x.numOfChildren;
+            phase.add(lastActivity);
+            return x.children.get(x.children.size() - 1);
         }
 
     }
 
     private Node findActivityNode(List<String> phase) {
         Node x = root;
+        boolean isFind = false;
         for (int i = 0; i < phase.size(); i++) {
-            for (int j = 0; j < x.numOfChildren; j++) {
-                Node child = x.children.get(i);
-                if (child.equals(phase.get(i))) {
+            for (int j = 0; j < x.children.size(); j++) {
+                Node child = x.children.get(j);
+                if (child.activity.equals(phase.get(i))) {
                     x = child;
+                    isFind = true;
                     break;
                 }
             }
-            if (x == null) return null;
+            if (!isFind) return root;
+            isFind = false;
         }
         return x;
+    }
+
+    public static void main(String[] args) {
+        ActiveLzTree alz = new ActiveLzTree();
+        alz.init();
+        String s = "a,a,a,b,a,b,b,b,b,b,a,a,b,c,c,d,d,c,b,a,a,a,a";
+        String[] ss = s.split(",");
+        for (int i = 0; i < ss.length; i++) {
+            alz.step(ss[i]);
+        }
     }
 
 }
